805b71de9e169417f0425fd5fc3005c764e16e0a
Navras Bokiev <navras96@gmail.com>
2018-02-28 17:39:58 +0500

    added Anko

--- app/src/main/java/com/example/pastkhuf/songbook/MainActivity.kt
+++ app/src/main/java/com/example/pastkhuf/songbook/MainActivity.kt
@@ -1,40 +1,43 @@
 package com.example.pastkhuf.songbook
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.DefaultItemAnimator
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import com.google.gson.Gson
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.jetbrains.anko.listView
+import org.jetbrains.anko.textView
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.verticalLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        verticalLayout{
+            val listView = listView()
 
-        val layoutManager = LinearLayoutManager(this)  //applicationContext
-        recyclerView.layoutManager = layoutManager
-        recyclerView.itemAnimator = DefaultItemAnimator()
+            Thread(Runnable {
+                val client = OkHttpClient()
+                val request = Request.Builder()
+                        .url("http://api.guitarparty.com/v2/songs/?query=Adam")
+                        .addHeader("Guitarparty-Api-Key",  "27b65ca0ece5d0cffa3e8fa9c3e3c41c20294f14")
+                        .build()
+                val response = client.newCall(request).execute()
+                val responseText = response.body()!!.string()
+                val repos = Gson().fromJson(responseText, SongInfo::class.java)
 
-        Thread(Runnable {
-            val client = OkHttpClient()
-            val request = Request.Builder()
-                    .url("http://api.guitarparty.com/v2/songs/?query=Jolene")
-                    .addHeader("Guitarparty-Api-Key",  "27b65ca0ece5d0cffa3e8fa9c3e3c41c20294f14")
-                    .build()
-            val response = client.newCall(request).execute()
-            val responseText = response.body()!!.string()
-            val repos = Gson().fromJson(responseText, SongInfo::class.java)
+                runOnUiThread{
+                    val adapter = SongAdapter2(repos.objects)
+                    listView.adapter = adapter
+                    adapter.notifyDataSetChanged()
+                }
+            }).start()
 
-            runOnUiThread{
-                val adapter = SongAdapter(repos.objects)
-                recyclerView.adapter = adapter
-                adapter.notifyDataSetChanged()
+            listView.setOnItemClickListener { parent, view, position, l ->
+                val song = parent.getItemAtPosition(position) as Song
+                toast(song.title)
             }
-        }).start()
+        }
     }
 }
---
+++ app/src/main/java/com/example/pastkhuf/songbook/SongAdapter2.kt
@@ -0,0 +1,52 @@
+package com.example.pastkhuf.songbook
+
+import android.annotation.SuppressLint
+import android.graphics.Typeface
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import org.jetbrains.anko.*
+
+class SongAdapter2(private val items: List<Song>): BaseAdapter() {
+    override fun getItemId(position: Int): Long {
+        return getItem(position).id
+    }
+
+    override fun getItem(position: Int): Song {
+        return items[position]
+    }
+
+    override fun getCount(): Int {
+        return items.size
+    }
+
+    @SuppressLint("ResourceType")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val song = getItem(position)
+
+        return  with(parent!!.context){
+            relativeLayout {
+                val songName = textView {
+                    id = 2
+                    textSize = 22F
+                    typeface = Typeface.DEFAULT_BOLD
+                }.lparams{
+                    alignParentTop()
+                    alignParentLeft()
+                    width = parent.width
+                }
+                val authorsName = textView {
+                    id = 3
+                    textSize = 15F
+                    typeface = Typeface.create("BOLD", Typeface.ITALIC)
+                }.lparams{
+                    below(2)
+                    alignParentRight()
+                    width = parent.width
+                }
+                songName.text = song.title
+                authorsName.text = song.authors.joinToString(",")
+            }
+        }
+    }
+}
