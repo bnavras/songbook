5ff3803ba14ea3ec8a05e776b41621d6af86da0f
Navras Bokiev <navras96@gmail.com>
2018-03-21 15:55:02 +0500

    added SongActivity

--- app/src/main/java/com/example/pastkhuf/songbook/SongActivity.kt
+++ app/src/main/java/com/example/pastkhuf/songbook/SongActivity.kt
@@ -2,28 +2,40 @@ package com.example.pastkhuf.songbook
 
 import android.graphics.Typeface
 import android.os.Bundle
-import android.support.v4.widget.TextViewCompat
 import android.support.v7.app.AppCompatActivity
-import org.jetbrains.anko.dip
-import org.jetbrains.anko.textView
-import org.jetbrains.anko.verticalLayout
+import org.jetbrains.anko.*
+import org.jetbrains.anko.support.v4.nestedScrollView
 
 class SongActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val song = intent.getSerializableExtra("song") as Song
         verticalLayout{
-            val songName = textView{
-                text = song.title
-                typeface = Typeface.DEFAULT_BOLD
-                width = maxWidth
-            }
-            TextViewCompat.setAutoSizeTextTypeUniformWithConfiguration(songName,10,30,2, 3)
             textView{
+                topPadding = dip(5)
+                text = song.title
+                textSize = 25f
+                //typeface = Typeface.DEFAULT_BOLD
+                bottomPadding = dip(10)
+            }.lparams(width = matchParent)
+
+            textView{
+                text = "Authors:"
+                textSize = 17f
+                typeface = Typeface.DEFAULT_BOLD
+            }.lparams{/*below(1)*/}
+
+            textView{
+                text = song.authors.joinToString("\n")
+                textSize = 15f
+                bottomPadding = dip(10)
+            }.lparams{ /*below(2)*/ }
+
+            textView{
+                textSize = 15f
                 text = song.body
-                setPadding(dip(2), dip(songName.paddingEnd + 20), dip(2), dip(2))
-            }
+                setPadding(2,10,2,2)
+            }.lparams{ /*below(3)*/ }
         }
     }
 }
