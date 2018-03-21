7d43ab77a00aa02fdf91cc303d97478c8513e6fd
Navras Bokiev <navras96@gmail.com>
2018-03-21 16:09:13 +0500

    added SongActivity

--- app/src/main/java/com/example/pastkhuf/songbook/SongActivity.kt
+++ app/src/main/java/com/example/pastkhuf/songbook/SongActivity.kt
@@ -10,32 +10,35 @@ class SongActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val song = intent.getSerializableExtra("song") as Song
-        verticalLayout{
-            textView{
-                topPadding = dip(5)
-                text = song.title
-                textSize = 25f
-                //typeface = Typeface.DEFAULT_BOLD
-                bottomPadding = dip(10)
-            }.lparams(width = matchParent)
+        nestedScrollView{
+            verticalLayout{
+                textView{
+                    topPadding = dip(5)
+                    text = song.title
+                    textSize = 25f
+                    //typeface = Typeface.DEFAULT_BOLD
+                    bottomPadding = dip(10)
+                }.lparams(width = matchParent)
 
-            textView{
-                text = "Authors:"
-                textSize = 17f
-                typeface = Typeface.DEFAULT_BOLD
-            }.lparams{/*below(1)*/}
+                textView{
+                    text = "Authors:"
+                    textSize = 17f
+                    typeface = Typeface.DEFAULT_BOLD
+                }.lparams{/*below(1)*/}
 
-            textView{
-                text = song.authors.joinToString("\n")
-                textSize = 15f
-                bottomPadding = dip(10)
-            }.lparams{ /*below(2)*/ }
+                textView{
+                    text = song.authors.joinToString("\n")
+                    textSize = 15f
+                    bottomPadding = dip(10)
+                }.lparams{ /*below(2)*/ }
 
-            textView{
-                textSize = 15f
-                text = song.body
-                setPadding(2,10,2,2)
-            }.lparams{ /*below(3)*/ }
+                textView{
+                    textSize = 15f
+                    text = song.body
+                    setPadding(2,10,2,2)
+                }.lparams{ /*below(3)*/ }
+            }
         }
+
     }
 }
